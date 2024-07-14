cee28edb2ef3b8daa777bc1045f62e8ec7e3a5b4
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2024-07-14 15:54:59 +0200

    own endpoints

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
-const { Keypair, Connection, clusterApiUrl, PublicKey } = require("@solana/web3.js");
+const { Keypair, Connection, PublicKey } = require("@solana/web3.js");
 const fs = require('fs');
 
 let payer = null;
 const SOLANA_WALLET_PATH = process.env.SOLANA_WALLET_PATH;
+const SOLANA_WSS_ENDPOINT = process.env.SOLANA_WSS_ENDPOINT;
+const SOLANA_HTTP_ENDPOINT = process.env.SOLANA_HTTP_ENDPOINT;
 
 if (!fs.existsSync(SOLANA_WALLET_PATH)) {
   console.log("Generating a new keypair");
@@ -36,7 +38,7 @@ if (!fs.existsSync(SOLANA_WALLET_PATH)) {
 // console.log("Public Key:", payer.publicKey.toString());
 // console.log("Secret Key:", payer.secretKey)
 
-const connection = new Connection(clusterApiUrl('mainnet-beta'));
+const connection = new Connection(SOLANA_HTTP_ENDPOINT);
 
 const fetchSPLTokens = async () => {
   const tokenAccounts = await connection.getTokenAccountsByOwner(payer.publicKey, { programId: new PublicKey("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA") });
