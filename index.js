d9b6768505d2215babc3fc872b546baf95b2a831
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2024-07-14 14:57:45 +0200

    setup payer wallet

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 require('dotenv').config();
-const solanaWeb3 = require("@solana/web3.js");
 
-const generateKey = async () => {
-  const keyPair = solanaWeb3.Keypair.generate();
+const { Keypair } = require("@solana/web3.js");
 
-  console.log("Public Key:", keyPair.publicKey.toString());
-  console.log("Secret Key:", keyPair.secretKey)
-};
+const fs = require('fs');
 
-generateKey();
+// setup payer wallet
+
+let payer = null;
+
+if (!fs.existsSync('./key.json')) {
+
+  // if key.json doesn't exist, create a new keypair and save it to key.json
+
+  payer = Keypair.generate();
+
+  fs.writeFileSync('./key.json', JSON.stringify(Array.from(payer.secretKey)));
+
+} else {
+
+  // if key.json exists, read the secret key from key.json
+
+  const secretKey = fs.readFileSync('./key.json', 'utf8');
+
+  payer = Keypair.fromSecretKey(Uint8Array.from(JSON.parse(secretKey)));
+
+}
+
+console.log("Public Key:", payer.publicKey.toString());
+
+console.log("Secret Key:", payer.secretKey)
