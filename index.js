a7a84e3d36afbacc72b63d5155846d7eead780b1
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2024-07-14 20:16:43 +0200

    listen new tokens created

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const SOLANA_WALLET_PATH = process.env.SOLANA_WALLET_PATH;
 const SOLANA_WSS_ENDPOINT = process.env.SOLANA_WSS_ENDPOINT;
 const SOLANA_HTTP_ENDPOINT = process.env.SOLANA_HTTP_ENDPOINT;
 
+// setup wallet
+
 if (!fs.existsSync(SOLANA_WALLET_PATH)) {
   console.log("Generating a new keypair");
 
@@ -35,10 +37,32 @@ if (!fs.existsSync(SOLANA_WALLET_PATH)) {
   }
 }
 
-// console.log("Public Key:", payer.publicKey.toString());
-// console.log("Secret Key:", payer.secretKey)
+// setup connection
 
 const connection = new Connection(SOLANA_HTTP_ENDPOINT, {wsEndpoint: SOLANA_WSS_ENDPOINT});
+
+var WebSocket = require('ws');
+var ws = new WebSocket('wss://frontend-api.pump.fun/socket.io/?EIO=4&transport=websocket');
+
+ws.on('open', function() {
+    console.log('Socket opened!')
+});
+
+ws.on('message', function(data, flags) {
+    const message = data.toString()
+    if(message.startsWith('0')){
+        ws.send(40)
+    }else if(message === '2'){
+        ws.send(3)
+        console.log('Pinging...')
+    }else if(message.startsWith('42')) {
+        const parsed = JSON.parse(message.slice(2))
+        if(parsed[0]==='newCoinCreated'){
+            console.log('New token created!')
+            console.log(`https://pump.fun/${parsed[1].mint} ${parsed[1].name}`)
+        }
+    }
+});
 
 const fetchSPLTokens = async () => {
   const tokenAccounts = await connection.getTokenAccountsByOwner(payer.publicKey, { programId: new PublicKey("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA") });
@@ -52,15 +76,15 @@ const checkBalance = async () => {
 
 checkBalance();
 
-(async()=>{
-    const ACCOUNT_TO_WATCH = new PublicKey('CKu1F5sWUTeEy3NaBc2TxAK9NFRCS5srjFhPynXZ2ENt'); // Replace with your own Wallet Address
-    const subscriptionId = await connection.onAccountChange(
-        ACCOUNT_TO_WATCH,
-        (updatedAccountInfo) =>
-            console.log(`---Event Notification for ${ACCOUNT_TO_WATCH.toString()}--- \nNew Account Balance:`, updatedAccountInfo.lamports / LAMPORTS_PER_SOL, ' SOL'),
-        "confirmed"
-    );
-    console.log('Starting web socket, subscription ID: ', subscriptionId);
-})()
+// (async()=>{
+//     const ACCOUNT_TO_WATCH = new PublicKey('CKu1F5sWUTeEy3NaBc2TxAK9NFRCS5srjFhPynXZ2ENt'); // Replace with your own Wallet Address
+//     const subscriptionId = await connection.onAccountChange(
+//         ACCOUNT_TO_WATCH,
+//         (updatedAccountInfo) =>
+//             console.log(`---Event Notification for ${ACCOUNT_TO_WATCH.toString()}--- \nNew Account Balance:`, updatedAccountInfo.lamports / LAMPORTS_PER_SOL, ' SOL'),
+//         "confirmed"
+//     );
+//     console.log('Starting web socket, subscription ID: ', subscriptionId);
+// })()
 
 // fetchSPLTokens();
