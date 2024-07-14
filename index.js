01e32418edec7c3ef54243da928e8f9175dd0482
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2024-07-14 15:10:31 +0200

    catch errors

--- index.js
+++ index.js
@@ -1,33 +1,38 @@
 require('dotenv').config();
-
 const { Keypair } = require("@solana/web3.js");
-
 const fs = require('fs');
 
-// setup payer wallet
-
 let payer = null;
-
 const SOLANA_WALLET_PATH = process.env.SOLANA_WALLET_PATH;
 
 if (!fs.existsSync(SOLANA_WALLET_PATH)) {
-
   console.log("Generating a new keypair");
 
   payer = Keypair.generate();
 
   fs.writeFileSync(SOLANA_WALLET_PATH, JSON.stringify(Array.from(payer.secretKey)));
 
+  console.log(`Keypair saved to ${SOLANA_WALLET_PATH}`);
 } else {
-
   console.log(`Reading keypair from ${SOLANA_WALLET_PATH}`);
+  try {
 
-  const secretKey = fs.readFileSync(SOLANA_WALLET_PATH, 'utf8');
+    const keypair = fs.readFileSync(SOLANA_WALLET_PATH, 'utf8');
+    const keypairArray = JSON.parse(keypair);
 
-  payer = Keypair.fromSecretKey(Uint8Array.from(JSON.parse(secretKey)));
+    if (Array.isArray(keypairArray)) {
+      const privateKey = Uint8Array.from(keypairArray);
+      payer = Keypair.fromSecretKey(privateKey);
 
+      console.log('Private key loaded from keypair file');
+    } else {
+      throw new Error('Invalid keypair format');
+    }
+  } catch (error) {
+    console.error('Error reading Solana wallet keypair:', error);
+    process.exit(1);
+  }
 }
 
 console.log("Public Key:", payer.publicKey.toString());
-
 console.log("Secret Key:", payer.secretKey)
