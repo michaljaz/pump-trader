dc127320851a97e35017200df98293b351d8debe
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2024-07-15 12:46:27 +0200

    disable wallet config

--- index.js
+++ index.js
@@ -5,48 +5,48 @@ import { Keypair, Connection, PublicKey } from "@solana/web3.js";
 
 dotenv.config();
 
-const SOLANA_WALLET_PATH = process.env.SOLANA_WALLET_PATH;
-const SOLANA_WSS_ENDPOINT = process.env.SOLANA_WSS_ENDPOINT;
-const SOLANA_HTTP_ENDPOINT = process.env.SOLANA_HTTP_ENDPOINT;
+// const SOLANA_WALLET_PATH = process.env.SOLANA_WALLET_PATH;
+// const SOLANA_WSS_ENDPOINT = process.env.SOLANA_WSS_ENDPOINT;
+// const SOLANA_HTTP_ENDPOINT = process.env.SOLANA_HTTP_ENDPOINT;
 
 // setup wallet
 
-let privateKey;
-let payer;
+// let privateKey;
+// let payer;
 
-if (!fs.existsSync(SOLANA_WALLET_PATH)) {
-  console.log("Generating a new keypair...");
+// if (!fs.existsSync(SOLANA_WALLET_PATH)) {
+//   console.log("Generating a new keypair...");
 
-  payer = Keypair.generate();
+//   payer = Keypair.generate();
 
-  privateKey = payer.secretKey;
+//   privateKey = payer.secretKey;
 
-  fs.writeFileSync(SOLANA_WALLET_PATH, JSON.stringify(Array.from(privateKey)));
+//   fs.writeFileSync(SOLANA_WALLET_PATH, JSON.stringify(Array.from(privateKey)));
 
-  console.log(`Keypair saved to ${SOLANA_WALLET_PATH}`);
-} else {
-  console.log(`Reading keypair...`);
-  try {
-    const keypair = fs.readFileSync(SOLANA_WALLET_PATH, 'utf8');
-    const keypairArray = JSON.parse(keypair);
+//   console.log(`Keypair saved to ${SOLANA_WALLET_PATH}`);
+// } else {
+//   console.log(`Reading keypair...`);
+//   try {
+//     const keypair = fs.readFileSync(SOLANA_WALLET_PATH, 'utf8');
+//     const keypairArray = JSON.parse(keypair);
 
-    if (Array.isArray(keypairArray)) {
-      privateKey = Uint8Array.from(keypairArray);
-      payer = Keypair.fromSecretKey(privateKey);
+//     if (Array.isArray(keypairArray)) {
+//       privateKey = Uint8Array.from(keypairArray);
+//       payer = Keypair.fromSecretKey(privateKey);
 
-      console.log('Private key loaded from keypair file');
-    } else {
-      throw new Error('Invalid keypair format');
-    }
-  } catch (error) {
-    console.error('Error reading Solana wallet keypair:', error);
-    process.exit(1);
-  }
-}
+//       console.log('Private key loaded from keypair file');
+//     } else {
+//       throw new Error('Invalid keypair format');
+//     }
+//   } catch (error) {
+//     console.error('Error reading Solana wallet keypair:', error);
+//     process.exit(1);
+//   }
+// }
 
 // setup connection
 
-const connection = new Connection(SOLANA_HTTP_ENDPOINT, {wsEndpoint: SOLANA_WSS_ENDPOINT});
+// const connection = new Connection(SOLANA_HTTP_ENDPOINT, {wsEndpoint: SOLANA_WSS_ENDPOINT});
 const ws = new WebSocket('wss://frontend-api.pump.fun/socket.io/?EIO=4&transport=websocket');
 
 // spy on pump.fun websocket and then subscribe to the token
