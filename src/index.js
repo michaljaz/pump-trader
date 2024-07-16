3bb52126ee649f6089ab14c6712edccace6d2c2e
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2024-07-16 22:55:49 +0200

    associatedBondingCurve

--- src/index.js
+++ src/index.js
@@ -5,13 +5,14 @@ import fetch from 'node-fetch';
 import bs58 from 'bs58';
 import { Program, AnchorProvider, setProvider, Wallet } from '@coral-xyz/anchor';
 import { Keypair, Connection, PublicKey, VersionedTransaction } from "@solana/web3.js";
+import { TOKEN_PROGRAM_ID, ASSOCIATED_TOKEN_PROGRAM_ID } from '@solana/spl-token';
 
 dotenv.config();
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const SOLANA_WSS_ENDPOINT = process.env.SOLANA_WSS_ENDPOINT;
 const SOLANA_HTTP_ENDPOINT = process.env.SOLANA_HTTP_ENDPOINT;
-const PUMP_FUN_PROGRAM_ID = '6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P';
+const PUMP_FUN_PROGRAM = new PublicKey('6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P');
 
 console.log(`Reading keypair...`);
 const payer = Keypair.fromSecretKey(bs58.decode(PRIVATE_KEY));
@@ -22,12 +23,14 @@ console.log(payer.publicKey.toString());
 
 const buyTransaction = (mintAddress, amount) => {
   console.log('Mint address:', mintAddress);
-  const programId = new PublicKey(PUMP_FUN_PROGRAM_ID);
   const wallet = new Wallet(payer);
   const mint = new PublicKey(mintAddress);
 
-  const [bondingCurve] = PublicKey.findProgramAddressSync([Buffer.from("bonding-curve"), new PublicKey(mintAddress).toBytes()], programId);
+  const [bondingCurve] = PublicKey.findProgramAddressSync([Buffer.from("bonding-curve"), new PublicKey(mintAddress).toBytes()], PUMP_FUN_PROGRAM);
   console.log('Bonding curve:', bondingCurve.toString());
+
+  const [associatedBondingCurve] = PublicKey.findProgramAddressSync([bondingCurve.toBuffer(), PUMP_FUN_PROGRAM.toBytes(), new PublicKey(mintAddress).toBytes()], ASSOCIATED_TOKEN_PROGRAM_ID);
+  console.log('Associated bonding curve:', associatedBondingCurve.toString());
 }
 
 const checkBalance = async () => {
