961fdc4d0813327fd1a645167dbd23f363983567
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2024-07-16 14:56:18 +0200

    use pumpportal

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 Simple and smart solana bot, that trades tokens on pump.fun.
 
 ## Which connections bot uses?
-- *Pump.fun websocket*: to receive info about new tokens & trades
-- *WSS and HTTPS SOLANA RPC*: to send realtime transactions
+- *pumpportal.fun*: to receive info about new tokens & trades
+- *WSS and HTTPS SOLANA RPC*: to check current account balance
--- index.js
+++ index.js
@@ -5,86 +5,64 @@ import { Keypair, Connection, PublicKey } from "@solana/web3.js";
 
 dotenv.config();
 
-// const SOLANA_WALLET_PATH = process.env.SOLANA_WALLET_PATH;
-// const SOLANA_WSS_ENDPOINT = process.env.SOLANA_WSS_ENDPOINT;
-// const SOLANA_HTTP_ENDPOINT = process.env.SOLANA_HTTP_ENDPOINT;
+const SOLANA_WALLET_PATH = process.env.SOLANA_WALLET_PATH;
+const SOLANA_WSS_ENDPOINT = process.env.SOLANA_WSS_ENDPOINT;
+const SOLANA_HTTP_ENDPOINT = process.env.SOLANA_HTTP_ENDPOINT;
 
-// setup wallet
+let privateKey;
+let payer;
 
-// let privateKey;
-// let payer;
+if (!fs.existsSync(SOLANA_WALLET_PATH)) {
+  console.log("Generating a new keypair...");
 
-// if (!fs.existsSync(SOLANA_WALLET_PATH)) {
-//   console.log("Generating a new keypair...");
+  payer = Keypair.generate();
 
-//   payer = Keypair.generate();
+  privateKey = payer.secretKey;
 
-//   privateKey = payer.secretKey;
+  fs.writeFileSync(SOLANA_WALLET_PATH, JSON.stringify(Array.from(privateKey)));
 
-//   fs.writeFileSync(SOLANA_WALLET_PATH, JSON.stringify(Array.from(privateKey)));
+  console.log(`Keypair saved to ${SOLANA_WALLET_PATH}`);
+} else {
+  console.log(`Reading keypair...`);
+  try {
+    const keypair = fs.readFileSync(SOLANA_WALLET_PATH, 'utf8');
+    const keypairArray = JSON.parse(keypair);
 
-//   console.log(`Keypair saved to ${SOLANA_WALLET_PATH}`);
-// } else {
-//   console.log(`Reading keypair...`);
-//   try {
-//     const keypair = fs.readFileSync(SOLANA_WALLET_PATH, 'utf8');
-//     const keypairArray = JSON.parse(keypair);
+    if (Array.isArray(keypairArray)) {
+      privateKey = Uint8Array.from(keypairArray);
+      payer = Keypair.fromSecretKey(privateKey);
 
-//     if (Array.isArray(keypairArray)) {
-//       privateKey = Uint8Array.from(keypairArray);
-//       payer = Keypair.fromSecretKey(privateKey);
+      console.log('Private key loaded from keypair file');
+    } else {
+      throw new Error('Invalid keypair format');
+    }
+  } catch (error) {
+    console.error('Error reading Solana wallet keypair:', error);
+    process.exit(1);
+  }
+}
 
-//       console.log('Private key loaded from keypair file');
-//     } else {
-//       throw new Error('Invalid keypair format');
-//     }
-//   } catch (error) {
-//     console.error('Error reading Solana wallet keypair:', error);
-//     process.exit(1);
-//   }
-// }
+const connection = new Connection(SOLANA_HTTP_ENDPOINT, {wsEndpoint: SOLANA_WSS_ENDPOINT});
+const ws = new WebSocket('wss://pumpportal.fun/api/data');
 
-// setup connection
-
-// const connection = new Connection(SOLANA_HTTP_ENDPOINT, {wsEndpoint: SOLANA_WSS_ENDPOINT});
-const ws = new WebSocket('wss://frontend-api.pump.fun/socket.io/?EIO=4&transport=websocket');
-
-// spy on pump.fun websocket and then subscribe to the token
 
 ws.on('open', function() {
-    console.log('Listening for new token creation on pump.fun...')
+  ws.send(JSON.stringify({ "method": "subscribeNewToken" }))
+  console.log('Connected to pumpportal.fun');
 });
 
 let spying = false
 
-ws.on('message', function(data, flags) {
-  const message = data.toString()
-  if(message.startsWith('0')){
-    ws.send(40)
-  }else if(message === '2'){
-    ws.send(3)
-  }else if(message.startsWith('42')) {
-    const [type, {
-      mint, name, is_buy, sol_amount, username, signature, token_amount
-    }] = JSON.parse(message.slice(2))
+ws.on('message', function (data, flags) {
+  data = JSON.parse(data.toString())
+  
+  if (!data.message && !spying) {
+    spying = data.mint
+    console.log(`https://pump.fun/${data.mint}`)
 
-    
-
-    
-    if(type === 'newCoinCreated' && !spying){
-      console.log('New token created!')
-      console.log(`https://pump.fun/${mint} ${name}`)
-      spying = mint
-      ws.send('42["joinTradeRoom",{"mint":"'+spying+'"}]')
-    } else if ((type === `tradeCreated:${spying}` || (type === 'tradeCreated' && mint === spying))) {
-      // console.log(JSON.parse(message.slice(2))[1])
-      const sig = signature.slice(0, 6)
-      console.log(
-        is_buy ? 'BUY\t' : 'SELL\t',
-        `"${name}" at ${(sol_amount * 0.000000001).toFixed(4)} SOL`,
-        `(${((sol_amount * 0.000000001) / (token_amount / 1e6))})`,
-        sig
-      )
-    }
+    ws.send(JSON.stringify({"method": "unsubscribeNewToken"}))
+    ws.send(JSON.stringify({"method": "subscribeTokenTrade", "keys": [spying]}))
+  } else {
+    console.log(data)
   }
 });
